faeb5ccf1c8ba7083ccb68a77e0060dbae254191
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-12 16:07:53 +0100

    feat: add structures for board representation

--- game-engine/src/main.rs
+++ game-engine/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let b = Board {field: [
+        Field::Occ(Symbol::O), Field::Empty, Field::Empty, 
+        Field::Empty, Field::Occ(Symbol::X), Field::Empty,
+        Field::Empty, Field::Empty, Field::Empty]};
+    for a in b.field {
+        if let Field::Occ(a) = a {
+            if a == Symbol::X {
+                print!("X");
+            } else {
+                print!("O");
+            }
+        } else {
+            print!("_");
+        }
+    }
+
+}
+
+#[derive(PartialEq, Eq)]
+enum Symbol {
+    X,
+    O
+}
+
+enum Field {
+    Empty,
+    Occ(Symbol)
+}
+
+struct Board {
+    field: [Field; 9],
 }
