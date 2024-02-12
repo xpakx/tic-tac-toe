0303751ce83ddbe92f951ab7b4a8a9dd565ffde5
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-12 16:22:09 +0100

    feat(engine): add bitboards

--- game-engine/src/main.rs
+++ game-engine/src/main.rs
@@ -3,9 +3,9 @@ fn main() {
         Field::Occ(Symbol::O), Field::Empty, Field::Empty, 
         Field::Empty, Field::Occ(Symbol::X), Field::Empty,
         Field::Empty, Field::Empty, Field::Empty]};
-    for a in b.field {
+    for a in &b.field {
         if let Field::Occ(a) = a {
-            if a == Symbol::X {
+            if a == &Symbol::X {
                 print!("X");
             } else {
                 print!("O");
@@ -14,7 +14,10 @@ fn main() {
             print!("_");
         }
     }
-
+    println!("");
+    let bit = b.to_bitboard();
+    println!("{:09b}", bit.x);
+    println!("{:09b}", bit.o);
 }
 
 #[derive(PartialEq, Eq)]
@@ -30,4 +33,28 @@ enum Field {
 
 struct Board {
     field: [Field; 9],
+}
+
+struct BitBoard {
+    x: i32,
+    o: i32
+}
+
+impl Board{
+    fn to_bitboard(&self) -> BitBoard {
+        let mut bitboard_x = 0;
+        let mut bitboard_o = 0;
+        for a in &self.field {
+            bitboard_x = bitboard_x << 1;
+            bitboard_o = bitboard_o << 1;
+            if let Field::Occ(a) = a {
+                if a == &Symbol::X {
+                    bitboard_x += 1;
+                } else {
+                    bitboard_o += 1;
+                }
+            } 
+        }
+        return BitBoard {x: bitboard_x, o: bitboard_o};
+    }
 }
