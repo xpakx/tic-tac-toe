b14df714ef41dc1e636959a9a38aca2fbcc22677
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-13 14:09:27 +0100

    test(engine): test generating legal moves

--- game-engine/src/minmax.rs
+++ game-engine/src/minmax.rs
@@ -3,3 +3,29 @@ use crate::board::BitBoard;
 fn generate_moves(board: &BitBoard) -> i32 {
     return !(board.x | board.o) & 0b111111111;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_moves_empty_board() {
+        let board_empty = BitBoard { x: 0b000000000, o: 0b000000000 };
+        let moves_bitmask = generate_moves(&board_empty);
+        assert_eq!(moves_bitmask, 0b111111111);
+    }
+
+    #[test]
+    fn test_generate_moves_partial_board() {
+        let board_partial = BitBoard { x: 0b101001000, o: 0b010100000 };
+        let moves_bitmask = generate_moves(&board_partial);
+        assert_eq!(moves_bitmask, 0b000010111);
+    }
+
+    #[test]
+    fn test_generate_moves_full_board() {
+        let board_full = BitBoard { x: 0b111111111, o: 0b000000000 };
+        let moves_bitmask = generate_moves(&board_full);
+        assert_eq!(moves_bitmask, 0b000000000);
+    }
+}
