43acc5e4127490e87a5e6d5c176c14a8e7f71c5c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-13 15:09:54 +0100

    feat(engine): add minmax algorithm

--- game-engine/src/minmax.rs
+++ game-engine/src/minmax.rs
@@ -1,8 +1,86 @@
-use crate::board::BitBoard;
+use crate::board::{BitBoard, Symbol, check_win, check_draw};
 
 fn generate_moves(board: &BitBoard) -> i32 {
     return !(board.x | board.o) & 0b111111111;
 }
+
+pub fn min_max_decision(board: &BitBoard, player: &Symbol) -> i32 {
+    let moves = generate_moves(board);
+    let mut mask = 0b000000001;
+    let next_player = match player {
+        Symbol::X => Symbol::O,
+        Symbol::O => Symbol::X,
+    };
+    let mut best_move = 0;
+    let mut best_result = -1;
+    for _ in 0..9 {
+        if moves & mask != 0 {
+            let new_board = board.apply_move(&mask, player);
+            let min = min_value(&new_board, &next_player);
+            if min > best_result {
+                best_result = min;
+                best_move = mask;
+            }
+        }
+        mask = mask << 1;
+    }
+    return best_move;
+}
+
+fn max_value(board: &BitBoard, player: &Symbol) -> i8 {
+    if check_win(board).is_some() {
+        return -1;
+    }
+    if check_draw(board) {
+        return 0;
+    }
+    let moves = generate_moves(board);
+    let next_player = match player {
+        Symbol::X => Symbol::O,
+        Symbol::O => Symbol::X,
+    };
+    let mut best_result = -1;
+    let mut mask = 0b000000001;
+    for _ in 0..9 {
+        if moves & mask != 0 {
+            let new_board = board.apply_move(&mask, player);
+            let min = min_value(&new_board, &next_player);
+            if min > best_result {
+                best_result = min;
+            }
+        }
+        mask = mask << 1;
+    }
+    return best_result;
+}
+
+fn min_value(board: &BitBoard, player: &Symbol) -> i8 {
+    if check_win(board).is_some() {
+        return 1;
+    }
+    if check_draw(board) {
+        return 0;
+    }
+    let moves = generate_moves(board);
+    let next_player = match player {
+        Symbol::X => Symbol::O,
+        Symbol::O => Symbol::X,
+    };
+    let mut best_result = 1;
+    let mut mask = 0b000000001;
+    for _ in 0..9 {
+        if moves & mask != 0 {
+            let new_board = board.apply_move(&mask, player);
+            let max = max_value(&new_board, &next_player);
+            if max < best_result {
+                best_result = max;
+            }
+        }
+        mask = mask << 1;
+    }
+    return best_result;
+}
+
 
 #[cfg(test)]
 mod tests {
