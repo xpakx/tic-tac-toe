1d36ba95ac64117a3b0975f84b9d09124299c6f7
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-14 14:18:29 +0100

    chore(main): add user details service

---
+++ main/src/main/java/io/github/xpakx/tictactoe/user/UserService.java
@@ -0,0 +1,25 @@
+package io.github.xpakx.tictactoe.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var userAccount = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with username " + username));
+        return new User(String.valueOf(userAccount.getId()), userAccount.getPassword(), userAccount.getRoles());
+    }
+
+    public UserDetails userAccountToUserDetails(io.github.xpakx.tictactoe.user.User userAccount) {
+        return new User(String.valueOf(userAccount.getId()), userAccount.getPassword(), userAccount.getRoles());
+    }
+}
