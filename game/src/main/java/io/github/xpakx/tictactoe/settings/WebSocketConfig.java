1f9afc07251056fd3f206d02b971ef05c3b40c07
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-15 14:22:03 +0100

    chore(game): add config for websocket

---
+++ game/src/main/java/io/github/xpakx/tictactoe/settings/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package io.github.xpakx.tictactoe.settings;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config
+                .setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/play")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+}
