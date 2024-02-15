64500021ff7750b7772f7d3da969ba37dc57f17a
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-15 12:51:43 +0100

    chore(game): init service with websocket

---
+++ game/src/test/java/io/github/xpakx/tictactoe/TictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
