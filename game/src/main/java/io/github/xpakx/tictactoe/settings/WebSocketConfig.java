d0c3064db683c00fc0ada747445f71c41a190c52
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-15 15:39:27 +0100

    feat(game): get token form websocket headers and remember in session

--- game/src/main/java/io/github/xpakx/tictactoe/settings/WebSocketConfig.java
+++ game/src/main/java/io/github/xpakx/tictactoe/settings/WebSocketConfig.java
@@ -1,10 +1,22 @@
 package io.github.xpakx.tictactoe.settings;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -24,5 +36,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
-
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    MessageHeaders headers = message.getHeaders();
+                    MultiValueMap<String, String> multiValueMap = headers.get(
+                            StompHeaderAccessor.NATIVE_HEADERS,MultiValueMap.class);
+                    if (multiValueMap != null) {
+                        for(Map.Entry<String, List<String>> head : multiValueMap.entrySet())
+                        {
+                            if (head.getKey().equals("Token")) {
+                                final String token = head.getValue().get(0);
+                                accessor.setUser(() -> token); // TODO: use JWT
+                                break;
+                            }
+                        }
+                    }
+                }
+                return message;
+            }
+        });
+    }
 }
