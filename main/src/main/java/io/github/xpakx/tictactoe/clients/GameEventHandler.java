237a1287634b765f18cc11a621e1bda606ce3e6c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-17 13:16:17 +0100

    feat(main): process game requests from websocket service

---
+++ main/src/main/java/io/github/xpakx/tictactoe/clients/GameEventHandler.java
@@ -0,0 +1,28 @@
+package io.github.xpakx.tictactoe.clients;
+
+import io.github.xpakx.tictactoe.clients.event.GameEvent;
+import io.github.xpakx.tictactoe.game.GameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GameEventHandler {
+    private final StatePublisher publisher;
+    private final GameRepository repository;
+
+    @RabbitListener(queues = "${amqp.queue.games}")
+    void handleExpedition(final GameEvent event) {
+        try {
+            var game = repository.findById(event.getGameId());
+            game.ifPresent(publisher::sendGame);
+            if (game.isEmpty()) {
+                publisher.sendError("No such game!");
+            }
+        } catch (final Exception e) {
+            throw new AmqpRejectAndDontRequeueException(e);
+        }
+    }
+}
---
+++ main/src/main/java/io/github/xpakx/tictactoe/clients/event/GameEvent.java
@@ -0,0 +1,10 @@
+package io.github.xpakx.tictactoe.clients.event;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GameEvent {
+    private Long gameId;
+}
