b6fed2f0ad887ed1d78edaa765be938bf3d28700
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-17 14:08:16 +0100

    feat(game): add simple chat to game

--- game/src/main/java/io/github/xpakx/tictactoe/game/GameController.java
+++ game/src/main/java/io/github/xpakx/tictactoe/game/GameController.java
@@ -1,8 +1,6 @@
 package io.github.xpakx.tictactoe.game;
 
-import io.github.xpakx.tictactoe.game.dto.GameMessage;
-import io.github.xpakx.tictactoe.game.dto.MoveMessage;
-import io.github.xpakx.tictactoe.game.dto.MoveRequest;
+import io.github.xpakx.tictactoe.game.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -26,5 +24,14 @@ public class GameController {
     @SubscribeMapping("/topic/game/{id}")
     public GameMessage subscribe(@DestinationVariable Long id) {
         return service.subscribe(id);
+    }
+
+    @MessageMapping("/chat/{id}")
+    @SendTo("/topic/chat/{id}")
+    public ChatMessage chat(@DestinationVariable Long id, ChatRequest request, Principal principal) {
+        var msg = new ChatMessage();
+        msg.setPlayer(principal.getName());
+        msg.setMessage(request.getMessage());
+        return msg;
     }
 }
---
+++ game/src/main/java/io/github/xpakx/tictactoe/game/dto/ChatMessage.java
@@ -0,0 +1,11 @@
+package io.github.xpakx.tictactoe.game.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    private String player;
+    private String message;
+}
