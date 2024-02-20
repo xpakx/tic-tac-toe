5fb75fa258d425503e370af382c591fc0bbe4b47
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-20 12:26:25 +0100

    feat(frontend): let user accept/reject game requests

--- frontend/src/app/game-list/game-list.component.ts
+++ frontend/src/app/game-list/game-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Game } from '../main/dto/game';
+import { GameManagementService } from '../main/game-management.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-game-list',
@@ -11,17 +13,38 @@ export class GameListComponent implements OnInit {
   @Input() active: boolean = true;
   @Input() requests: boolean = false;
 
-  constructor() { }
+  constructor(private gameService: GameManagementService) { }
 
   ngOnInit(): void {
   }
 
   accept(gameId: number) {
+    this.gameService.acceptRequest(gameId, {accepted: true})
+      .subscribe({
+        next: (value: Boolean) => this.onAccept(gameId),
+        error: (err: HttpErrorResponse) => this.onError(err)
+      });
+  }
 
+  onAccept(gameId: number) {
+    this.open(gameId);
   }
 
   reject(gameId: number) {
+    this.gameService.acceptRequest(gameId, {accepted: false})
+      .subscribe({
+        next: (value: Boolean) => this.onReject(gameId),
+        error: (err: HttpErrorResponse) => this.onError(err)
+      });
 
+  }
+
+  onReject(gameId: number) {
+    // todo
+  }
+
+  onError(err: HttpErrorResponse) {
+    // todo
   }
 
   open(gameId: number) {
