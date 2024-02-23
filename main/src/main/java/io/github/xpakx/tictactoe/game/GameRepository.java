d02364a01702bfa224d06b0c01e26f7673c80e4d
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-23 14:47:23 +0100

    fix(main): fix fetching user/opponent with games list

--- main/src/main/java/io/github/xpakx/tictactoe/game/GameRepository.java
+++ main/src/main/java/io/github/xpakx/tictactoe/game/GameRepository.java
@@ -8,23 +8,23 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    @Query("select g, u1, u2 from Game g " +
-            "left join user u1 on g.user.id = u1.id " +
-            "left join user u2 on g.opponent.id = u2.id " +
+    @Query("select g from Game g " +
+            "left join fetch g.user " +
+            "left join fetch g.opponent " +
             "where g.opponent.id = ?1 and g.accepted = false and g.rejected = false")
     List<Game> findRequests(Long id);
 
-    @Query("select g, u1, u2 from Game g " +
-            "left join user u1 on g.user.id = u1.id " +
-            "left join user u2 on g.opponent.id = u2.id " +
+    @Query("select g from Game g " +
+            "left join fetch g.user " +
+            "left join fetch g.opponent " +
             "where " +
             "(g.user.id = ?1 or g.opponent.id = ?1) " +
             "and g.accepted = true and g.finished = false")
     List<Game> findActiveGames(Long id);
 
-    @Query("select g, u1, u2 from Game g " +
-            "left join user u1 on g.user.id = u1.id " +
-            "left join user u2 on g.opponent.id = u2.id " +
+    @Query("select g from Game g " +
+            "left join fetch g.user " +
+            "left join fetch g.opponent " +
             "where " +
             "(g.user.id = ?1 or g.opponent.id = ?1) " +
             "and g.accepted = true and g.finished = true")
