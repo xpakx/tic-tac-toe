024194fe0fb9d84969aca13bf255d17c8471dcc2
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-24 13:00:15 +0100

    fix(main): use more appropriate http error code

--- main/src/main/java/io/github/xpakx/tictactoe/game/error/RequestProcessedException.java
+++ main/src/main/java/io/github/xpakx/tictactoe/game/error/RequestProcessedException.java
@@ -5,7 +5,7 @@ package io.github.xpakx.tictactoe.game.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RequestProcessedException extends RuntimeException {
     public RequestProcessedException(String message) {
         super(message);
