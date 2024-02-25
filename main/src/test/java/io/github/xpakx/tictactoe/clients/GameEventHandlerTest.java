bc2e94cc3623375beb8bbf308097afbc855f99a8
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-25 12:11:56 +0100

    test(main): test publishing state events

--- main/src/test/java/io/github/xpakx/tictactoe/clients/GameEventHandlerTest.java
+++ main/src/test/java/io/github/xpakx/tictactoe/clients/GameEventHandlerTest.java
@@ -5,10 +5,9 @@ import io.github.xpakx.tictactoe.game.GameRepository;
 import io.github.xpakx.tictactoe.user.User;
 import io.github.xpakx.tictactoe.user.UserRepository;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.MockitoAnnotations;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +21,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
@@ -60,6 +60,26 @@ class GameEventHandlerTest {
     GameEventHandler gameHandler;
 
     Long userId;
+    private boolean rabbitSetupIsDone = false;
+
+    void config() {
+        var queue = new Queue("test.queue", true);
+        rabbitAdmin.declareQueue(queue);
+        var exchange = ExchangeBuilder
+                .topicExchange("tictactoe.state.topic")
+                .durable(true)
+                .build();
+        rabbitAdmin.declareExchange(exchange);
+        Binding binding = new Binding(
+                "test.queue",
+                Binding.DestinationType.QUEUE,
+                "tictactoe.state.topic",
+                "state",
+                null
+        );
+        rabbitAdmin.declareBinding(binding);
+        rabbitSetupIsDone = true;
+    }
 
     @BeforeEach
     void setUp() {
@@ -68,7 +88,9 @@ class GameEventHandlerTest {
         user.setPassword("password");
         user.setUsername("test_user");
         this.userId = userRepository.save(user).getId();
-
+        if (!rabbitSetupIsDone) {
+            config();
+        }
     }
 
     @AfterEach
@@ -101,9 +123,32 @@ class GameEventHandlerTest {
                 .until(isMessageConsumed(), Matchers.is(true));
     }
 
+    @Test
+    public void handlerShouldPublishEvent() {
+        var game = new GameEvent();
+        game.setGameId(5L);
+        rabbitTemplate.convertAndSend("tictactoe.games.topic", "game", game);
+        await()
+                .atMost(5, TimeUnit.SECONDS)
+                .until(isQueueNotEmpty("test.queue"), Matchers.is(true));
+    }
+
     private Callable<Boolean> isMessageConsumed() {
         return () ->
             mockingDetails(gameHandler).getInvocations().stream()
                     .anyMatch(invocation -> invocation.getMethod().getName().equals("handleGame"));
+    }
+
+    private int getMessageCount(String queueName) {
+        Properties queueProperties = rabbitAdmin.getQueueProperties(queueName);
+        if (queueProperties != null) {
+            return (int) queueProperties.get("QUEUE_MESSAGE_COUNT");
+        } else {
+            return 0;
+        }
+    }
+
+    private Callable<Boolean> isQueueNotEmpty(String queueName) {
+        return () -> getMessageCount(queueName) > 0;
     }
 }
