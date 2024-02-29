6b03f99c37fd72de3c6bec3aab2f7dca46faaecf
xpakx <55586470+xpakx@users.noreply.github.com>
2024-02-29 11:40:10 +0100

    test(game): test if game engine is asked for AI move after user's move

--- game/src/test/java/io/github/xpakx/tictactoe/game/EngineEventHandlerTest.java
+++ game/src/test/java/io/github/xpakx/tictactoe/game/EngineEventHandlerTest.java
@@ -1,10 +1,16 @@
 package io.github.xpakx.tictactoe.game;
 
 import com.redis.testcontainers.RedisContainer;
+import io.github.xpakx.tictactoe.clients.event.GameEvent;
+import io.github.xpakx.tictactoe.clients.event.MoveEvent;
 import io.github.xpakx.tictactoe.game.dto.EngineEvent;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +23,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
@@ -57,8 +67,31 @@ class EngineEventHandlerTest {
         registry.add("spring.data.redis.port", redis::getRedisPort);
     }
 
+    private boolean rabbitSetupIsDone = false;
+
+    void config() {
+        var queue = new Queue("test.queue", true);
+        rabbitAdmin.declareQueue(queue);
+        var exchange = ExchangeBuilder
+                .topicExchange("tictactoe.moves.topic")
+                .durable(true)
+                .build();
+        rabbitAdmin.declareExchange(exchange);
+        Binding binding = new Binding(
+                "test.queue",
+                Binding.DestinationType.QUEUE,
+                "tictactoe.moves.topic",
+                "move",
+                null
+        );
+        rabbitAdmin.declareBinding(binding);
+        rabbitSetupIsDone = true;
+    }
     @BeforeEach
     void setUp() {
+        if (!rabbitSetupIsDone) {
+            config();
+        }
     }
 
     @AfterEach
@@ -97,10 +130,64 @@ class EngineEventHandlerTest {
         assertThat(gameDb.getCurrentSymbol(), equalTo(GameSymbol.O));
     }
 
+    @Test
+    void shouldAskForAIMoveAfterUserMove() {
+        var game = new GameState();
+        game.setUsername1("user1");
+        game.setUser2AI(true);
+        game.setFirstUserStarts(true);
+        game.setId(5L);
+        game.setCurrentState("?????????");
+        game.setCurrentSymbol(GameSymbol.X);
+        gameRepository.save(game);
+
+        var event = new EngineEvent();
+        event.setGameId(5L);
+        event.setLegal(true);
+        event.setColumn(0);
+        event.setRow(0);
+        event.setNewState("X????????");
+        event.setMove("X????????");
+        rabbitTemplate.convertAndSend("tictactoe.engine.topic", "engine", event);
+        await()
+                .atMost(5, TimeUnit.SECONDS)
+                .until(isQueueNotEmpty("test.queue"), Matchers.is(true));
+        var moveOpt = getMoveMessage();
+        assertThat(moveOpt.isPresent(), is(true));
+        var move = moveOpt.get();
+        assertThat(move.isAi(), is(true));
+        assertThat(move.getGameState(), equalTo("X????????"));
+        assertThat(move.getCurrentSymbol(), equalTo("O"));
+    }
+
     private boolean recordHasLastMove(Long id) {
         var gameOpt = gameRepository.findById(id);
         return gameOpt
                 .map((a) -> Objects.nonNull(a.getLastMove()))
                 .orElse(false);
+    }
+
+    private int getMessageCount(String queueName) {
+        Properties queueProperties = rabbitAdmin.getQueueProperties(queueName);
+        if (queueProperties != null) {
+            return (int) queueProperties.get("QUEUE_MESSAGE_COUNT");
+        } else {
+            return 0;
+        }
+    }
+
+    private Callable<Boolean> isQueueNotEmpty(String queueName) {
+        return () -> getMessageCount(queueName) > 0;
+    }
+
+    private Optional<MoveEvent> getMoveMessage() {
+        var queuedMessage = rabbitTemplate.receiveAndConvert("test.queue");
+        if (Objects.isNull(queuedMessage)) {
+            return Optional.empty();
+        }
+        if (queuedMessage instanceof MoveEvent e) {
+            return Optional.of(e);
+        }
+        return Optional.empty();
     }
 }
