3fdba5a251830c90e4113ffd4e941efb43b405e7
xpakx <55586470+xpakx@users.noreply.github.com>
2024-03-02 12:28:10 +0100

    feat(frontend): add interceptor for expired jwt token

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ModalRegisterComponent } from './auth/modal-register/modal-register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from  '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from  '@angular/common/http';
 import { ModalLoginComponent } from './auth/modal-login/modal-login.component';
 import { BoardComponent } from './board/board.component';
 import { MenuComponent } from './menu/menu.component';
 import { GameListComponent } from './game-list/game-list.component';
 import { NewGameModalComponent } from './new-game-modal/new-game-modal.component';
 import { FieldPipe } from './board/field.pipe';
+import { ErrorInterceptor } from './error/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,14 @@ import { FieldPipe } from './board/field.pipe';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
